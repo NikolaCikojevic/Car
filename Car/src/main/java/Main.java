ba5176c74d61d270d352249ffb38c789bd4a5aef
NikolaCikojevic <nikolacikojevic3@gmail.com>
2021-03-13 15:38:43 +0100

    car

---
+++ Car/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Car audi = new Car("Audi", "A5", "Black", 2018);
+            audi.printAttributes();
+                
+    }
+    
+}
