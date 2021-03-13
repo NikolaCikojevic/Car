885bff9aabc2772821bdcfb9e7e850d7ab8f79b6
NikolaCikojevic <nikolacikojevic3@gmail.com>
2021-03-13 15:48:24 +0100

    car2

--- Car/src/main/java/Main.java
+++ Car/src/main/java/Main.java
@@ -7,6 +7,12 @@ public class Main {
         Car audi = new Car("Audi", "A5", "Black", 2018);
             audi.printAttributes();
                 
+            Car bmw = new Car("BMW", "X5", "Red", 2012);
+            bmw.printAttributes();
+           
+            
+            Car lada = new Car ("Lada", "Niva","Green", 2016);
+            lada.printAttributes();
     }
     
 }
